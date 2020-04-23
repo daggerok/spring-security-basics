52b00a6a922ed41397dc523f2c16f92179dc53a9
Maksim Kostromin <daggerok@gmail.com>
2020-04-23 23:57:24 +0300

    v1.0.5 release.

--- .mvn/release.sh
+++ .mvn/release.sh
@@ -7,18 +7,15 @@ if [[ "$currentVersion" == *"SNAPSHOT"* ]] ; then
     -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.nextIncrementalVersion}
   developmentVersion=`./mvnw -q --non-recursive exec:exec -Dexec.executable=echo -Dexec.args='${project.version}'`
   ./mvnw build-helper:parse-version -DgenerateBackupPoms=false versions:set -DnewVersion="$currentVersion"
-
   ./mvnw clean release:prepare release:perform -B \
       -Dgoals='package' -DgenerateReleasePoms=false -DgenerateBackupPoms=false \
       -DreleaseVersion="$currentVersion" -DdevelopmentVersion="$developmentVersion"
 else
   git tag "v$currentVersion"
-
   ./mvnw build-helper:parse-version -DgenerateBackupPoms=false -DgenerateBackupPoms=false versions:set \
     -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.nextIncrementalVersion}
   ./mvnw build-helper:parse-version -DgenerateBackupPoms=false -DgenerateBackupPoms=false versions:set \
     -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.nextIncrementalVersion} \
     -f step-4-java-ee-jaxrs-jboss-spring-security
-
   git add . ; git commit -am "v$currentVersion release." ; git push --tags
 fi
